9f251e83566b59999d7a608339c80e2a12c5c132
IvanTsiak <ivanzjak2007@gmail.com>
2025-10-19 20:00:03 +0300

    multithreads_any_of implementation, add best_k and detailed output

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <chrono>
 #include <format>
 #include <execution>
+#include <mutex>
+#include <atomic>
 
 std::vector<int> generate_random_data(size_t size) {
     std::vector<int> data(size);
@@ -19,16 +21,40 @@ std::vector<int> generate_random_data(size_t size) {
     return data;
 }
 
-bool multithreads_any_of(std::vector<int> data, std::function<bool(int)> predicate, unsigned int K) {
+template<typename Predicate>
+bool multithreads_any_of(const std::vector<int>& data, Predicate predicate, int K) {
     if (K==0) {
         return false;
     }
-    /*Тут потім буде така прекрасна реалізація такого прекрасного коду,
-    взагалі зроблю таке-то-таке, що й сам не знаю. Жартую.
-    Буде тут реалізація паралельного алгоритму, де кожному потоку дається своя частинка для
-    перевірки чи є якесь значення у векторі. Можливо, доцільно тут використати м'ютекс,
-    щоб уникнути стану гонки, але доживем і тоді побачим.*/
-    return true;
+
+    if (K==1) {
+        return std::any_of(data.begin(), data.end(), predicate);
+    }
+
+    std::atomic<bool> found{false};
+    {
+        std::vector<std::jthread> threads;
+        threads.reserve(K);
+        size_t chunk_size = data.size() / K;
+
+        for (int i=0; i<K; ++i) {
+            auto start = data.begin() + i * chunk_size;
+            auto end = (i==K-1) ? data.end() : start + chunk_size;
+
+            threads.emplace_back([&found, start, end, predicate] {
+                for (auto it = start; it != end; ++it) {
+                    if (found.load(std::memory_order_acquire)) {
+                        return;
+                    }
+                    if (predicate(*it)) {
+                        found.store(true, std::memory_order_release);
+                        return;
+                    }
+                }
+            });
+        }
+    }   
+    return found.load();
 }
 
 template <typename Func>
@@ -42,45 +68,74 @@ long long measure_time(Func func) {
     return duration;
 }
 
-void run_experiment(size_t data_size) {
-    std::vector<int> data = generate_random_data(data_size);
-    int value_to_find = -1;
+void run_experiment(const std::vector<int>& data, const int& value_to_find) {
+    // std::vector<int> data = generate_random_data(data_size);
+    // int value_to_find = -1;
     auto predicate = [value_to_find](int val) {
         return val == value_to_find;
     };
 
+    volatile bool res;
+    int op_tr = 0;
     //1
-    long long m_t = measure_time([data, predicate]() {
-        std::any_of(data.begin(), data.end(), predicate);
+    long long m_t = measure_time([&data, predicate, &res]() {
+        res = std::any_of(data.begin(), data.end(), predicate);
     });
 
     std::cout << "\nWithout politics: " << m_t << std::endl;
 
     //2
-    m_t = measure_time([data, predicate]() {
-        std::any_of(std::execution::seq, data.begin(), data.end(), predicate);
+    m_t = measure_time([&data, predicate, &res]() {
+        res = std::any_of(std::execution::seq, data.begin(), data.end(), predicate);
+    });;
+
+    std::cout << "Politic std::execution::seq: " << m_t << std::endl;
+
+    m_t = measure_time([&data, predicate, &res]() {
+        res = std::any_of(std::execution::par, data.begin(), data.end(), predicate);
     });
 
-    std::cout << "\nPolitic std::execution::seq: " << m_t << std::endl;
+    std::cout << "Politics std::execution::par: " << m_t << std::endl;
 
-    m_t = measure_time([data, predicate]() {
-        std::any_of(std::execution::par, data.begin(), data.end(), predicate);
+    m_t = measure_time([&data, predicate, &res]() {
+        res = std::any_of(std::execution::par_unseq, data.begin(), data.end(), predicate);
     });
 
-    std::cout << "\nPolitics std::execution::par: " << m_t << std::endl;
+    std::cout << "Politics std::execution::par_unseq: " << m_t << std::endl << std::endl;
 
-    m_t = measure_time([data, predicate]() {
-        std::any_of(std::execution::par_unseq, data.begin(), data.end(), predicate);
-    });
-
-    std::cout << "\nPolitics std::execution::par_unseq: " << m_t << std::endl;
     //3
-    
+    const int core_count = std::thread::hardware_concurrency();
+    long long best_time = -1;
+    int best_K = 0;
 
+    std::cout << "Speed of own parallel algorithm with different number of threads (K):" << std::endl;
+    for (int k=1; k<=core_count*2+1; ++k) {
+        bool res_custom;
+        long long time_custom = measure_time([&res_custom, &data, &predicate, k]{
+            res_custom = multithreads_any_of(data, predicate, k);
+        });
+        if (best_time == -1 || time_custom < best_time) {
+            best_time = time_custom;
+            best_K = k;
+        }
+
+        std::cout << "K = "<< k << ": " << time_custom << std::endl;
+    }
+
+    std::cout << "Best performance at K = " << best_K << " (" << best_time << " microseconds)\n";
 }
 
 int main() {
-    run_experiment(100000000);
-
+    std::vector<size_t> data_sizes = {100'000, 1'000'000, 100'000'00, 100'000'000};
+    std::vector<int> value = {-10, -1};
+    for (size_t data_size : data_sizes) {
+        std::cout << "\n----------------------------------------------" << 
+                    "\nData size: " << data_size << std::endl;
+        std::vector<int> data = generate_random_data(data_size);
+        data[data_size/2] = value[0];
+        for (int val : value) {
+            run_experiment(data, val);
+        }
+    }
     return 0;
 }
